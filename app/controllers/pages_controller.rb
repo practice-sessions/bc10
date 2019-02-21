eef813c5af185b9dc080e2bf8f72c0297c889ccf
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-02-21 18:15:00 +0100

    Added volunteer view, route and controller action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
    def about
    end
+
+   def volunteer
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :volunteers
   get 'about',to: 'pages#about'
+  get 'volunteer',to: 'pages#volunteer'
   resources :contacts, only: :create
+  resources :volunteers
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
