d11e9b3d223b94d7a2dc334de6d9d34032003d5f
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-02-21 10:27:56 +0100

    Improved error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,18 @@ class ContactsController < ApplicationController
   def new
   @contact = Contact.new
   end
+
   def create
   @contact = Contact.new(contact_params)
     if @contact.save
-     redirect_to new_contact_path, notice: "Message sent."
+     flash[:success] = "Message sent"
+     redirect_to new_contact_path
     else
-     redirect_to new_contact_path, notice: "Error occured."
+      flash[:error] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
   end
+
 private
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)
