f65ca17c4ccaef03c0fc07542b87f18cbdce5437
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-04-13 20:01:53 +0100

    Added avatar column to Profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190330205341) do
+ActiveRecord::Schema.define(version: 20190413185724) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -28,8 +28,12 @@ ActiveRecord::Schema.define(version: 20190330205341) do
     t.string   "phone_number"
     t.string   "contact_email"
     t.text     "decription"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,6 +49,10 @@ ActiveRecord::Schema.define(version: 20190330205341) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
