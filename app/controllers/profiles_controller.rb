4790600b1a09170ed6ffa50a780e4f86c37a1837
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-04-17 11:09:17 +0100

    Added profile update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -25,6 +25,22 @@ class ProfilesController < ApplicationController
      @profile = @user.profile
    end
 
+   # PATCH to /users/:user_id/profile
+  def update
+    # Retrieve the user from the database
+    @user = User.find( params[:user_id] )
+    # Retrieve that user's profile
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save (update)
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+
   private
   def profile_params
     params.required(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :decription)
